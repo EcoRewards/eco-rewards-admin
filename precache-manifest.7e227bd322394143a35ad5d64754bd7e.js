8918561c0eaf771446b280f6cffc210d90fab033
Linus Norton <linusnorton@gmail.com>
2019-11-28 19:35:42 +0000

    Updates

---
+++ precache-manifest.7e227bd322394143a35ad5d64754bd7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d82a74cdca4a390571e38ef8aa4bb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6b158b3e9f50b48f883",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bac3f74f467a3fedcd96",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "b6b158b3e9f50b48f883",
+    "url": "/static/js/2.05258b75.chunk.js"
+  },
+  {
+    "revision": "bac3f74f467a3fedcd96",
+    "url": "/static/js/main.4b099bf7.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  }
+]);
