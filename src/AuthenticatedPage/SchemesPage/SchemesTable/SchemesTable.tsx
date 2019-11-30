d5aa46b5b23541a6201f4a68b791639c1fd6e1e8
Linus Norton <linusnorton@gmail.com>
2019-11-30 21:20:40 +0000

    Added data table plugin

--- src/AuthenticatedPage/SchemesPage/SchemesTable/SchemesTable.tsx
+++ src/AuthenticatedPage/SchemesPage/SchemesTable/SchemesTable.tsx
@@ -1,9 +1,24 @@
-import React from "react";
-import { SchemeRow } from "./SchemeRow/SchemeRow";
+import React, { useMemo } from "react";
 import { AxiosInstance } from "axios";
 import { SchemeJsonView } from "eco-rewards-hub";
+import { toSchemeId } from "eco-rewards-hub/dist/src/scheme/Scheme";
+import DataTable from "react-data-table-component";
+import styled from "styled-components";
+import "./SchemesTable.css";
 
 export const SchemesTable = ({ api, schemes}: SchemesTableProps) => {
+  const rows = schemes.map(s => ({
+    id: toSchemeId(s.id!),
+    name: s.name
+  }));
+
+  const [filterText, setFilterText] = React.useState('');
+  const [resetPaginationToggle] = React.useState(false);
+  const filteredItems = rows.filter(item => item.name && item.name.includes(filterText));
+  const subHeaderComponentMemo = useMemo(() => {
+    return <FilterComponent onFilter={(e: any) => setFilterText(e.target.value)} filterText={filterText} />;
+  }, [filterText, resetPaginationToggle]);
+
   return (
     <div className="card shadow mb-4">
       <div className="card-header py-3">
@@ -11,23 +26,49 @@ export const SchemesTable = ({ api, schemes}: SchemesTableProps) => {
       </div>
       <div className="card-body">
         <div className="table-responsive">
-          <table className="table table-bordered" id="dataTable">
-            <thead>
-            <tr>
-              <th className="col-1">ID</th>
-              <th>Name</th>
-              <th className="col-1">Edit</th>
-            </tr>
-            </thead>
-            <tbody>
-            { schemes.map(s => <SchemeRow key={s.id} api={api} scheme={s}/>)}
-            </tbody>
-          </table>
+          <DataTable
+            columns={[{
+              name: "ID",
+              selector: "id",
+              sortable: true,
+              width: "100px"
+            },{
+              name: "Name",
+              selector: "name",
+              sortable: true
+            }]}
+            data={filteredItems}
+            pagination
+            paginationResetDefaultPage={resetPaginationToggle}
+            subHeader
+            subHeaderComponent={subHeaderComponentMemo}
+          />
         </div>
       </div>
     </div>
   );
 };
+
+const FilterComponent = ({ filterText, onFilter, onClear }: any) => (
+  <TextField id="search" type="text" placeholder="Filter By Name" value={filterText} onChange={onFilter} />
+);
+
+
+const TextField = styled.input`
+  height: 32px;
+  width: 200px;
+  border-radius: 3px;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+  border: 1px solid #e5e5e5;
+  padding: 0 32px 0 16px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 interface SchemesTableProps {
   api: AxiosInstance,
