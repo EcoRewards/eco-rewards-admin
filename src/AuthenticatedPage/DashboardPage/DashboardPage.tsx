73debf2800e859a7d7a92cfe08a70fc2089d8cd2
Linus Norton <linusnorton@gmail.com>
2020-02-01 21:25:11 +0000

    updated dashboard

--- src/AuthenticatedPage/DashboardPage/DashboardPage.tsx
+++ src/AuthenticatedPage/DashboardPage/DashboardPage.tsx
@@ -1,115 +1,89 @@
 import React, { useEffect, useState } from "react";
 import { AxiosInstance } from "axios";
-import "react-vis/dist/style.css";
-import { Bar, BarChart, CartesianGrid, Legend, YAxis, Tooltip, XAxis } from "recharts";
+import { DashboardChart } from "./DashboardChart";
+import { ReportScopeSelect } from "./ReportScopeSelect";
 
 export const DashboardPage = ({ api }: DashboardPageProps) => {
-  const [apiData, setApiData] = useState();
+  const [scope, setScope] = useState("/global/0/report");
+  const [names, setNames] = useState();
+  const [carbonSaving, setCarbonSaving] = useState();
+  const [rewardPoints, setRewardPoints] = useState();
+  const [distance, setDistance] = useState();
+  const [groups, setGroups] = useState([]);
+  const [links, setLinks] = useState({});
+
+  useEffect(() => {
+    async function fetchGroups() {
+      const response = await api.get("/groups");
+
+      setGroups(response.data.data);
+      setLinks(response.data.links);
+    }
+
+    if (groups.length === 0) {
+      fetchGroups();
+    }
+  }, [api, groups]);
 
   useEffect(() => {
     async function fetchApiData() {
-      const response = await api.get("/scheme/6/report");
+      const response = await api.get(scope);
 
-      setApiData(response.data.data);
+      const carbonSaving = {} as Record<string, Record<string, number>>;
+      const rewardPoints = {} as Record<string, Record<string, number>>;
+      const distance = {} as Record<string, Record<string, number>>;
+      const newNames = {} as Record<string, true>;
+
+      for (const row of response.data.data) {
+        carbonSaving[row.date] = carbonSaving[row.date] || { date: row.date };
+        carbonSaving[row.date][row.name] = row.totalCarbonSaving;
+        distance[row.date] = distance[row.date] || { date: row.date };
+        distance[row.date][row.name] = row.totalDistance;
+        rewardPoints[row.date] = rewardPoints[row.date] || { date: row.date };
+        rewardPoints[row.date][row.name] = row.totalRewardsEarned;
+        newNames[row.name] = true;
+      }
+
+      setNames(Object.keys(newNames));
+      setCarbonSaving(Object.values(carbonSaving));
+      setDistance(Object.values(distance));
+      setRewardPoints(Object.values(rewardPoints));
     }
 
-    if (!apiData) {
+    if (!names) {
       fetchApiData();
     }
-  }, [api, apiData]);
+  }, [api, names, scope]);
+
+  const onScopeChange = (scope: string) => {
+    setScope(scope ? scope + "/report" : "/global/0/report");
+    setNames(null);
+  };
 
   return (
     <div className="container-fluid">
       <h1 className="h3 mb-2 text-gray-800">Dashboard</h1>
-      <div className="card shadow mb-4">
-        <div className="card-header py-3">
-          <h6 className="m-0 font-weight-bold text-primary">Overview</h6>
-        </div>
-        <div className="card-body">
-          { apiData ? renderChart2(apiData) : <p>Loading</p> }
-        </div>
-      </div>
-      <div className="card shadow mb-4">
-        <div className="card-header py-3">
-          <h6 className="m-0 font-weight-bold text-primary">Carbon Saving</h6>
-        </div>
-        <div className="card-body">
-          { apiData ? renderChart(apiData) : <p>Loading</p> }
-        </div>
-      </div>
+      <ReportScopeSelect groups={groups} links={links} onChange={onScopeChange}/>
+      { !names
+        ? <p>Loading</p>
+        : <>
+            <DashboardChart key="rewards" title="Rewards Earned" data={rewardPoints} names={names}/>
+            <DashboardChart key="carbon" title="Carbon Saving" data={carbonSaving} names={names}/>
+            <DashboardChart key="distance" title="Distance" data={distance} names={names}/>
+          </>
+      }
     </div>
   );
 };
 
-function renderChart2(apiData: any[]) {
-  const data = apiData.reduce((index, row) => {
-    index[row.name] = index[row.name] || { name: row.name, totalCarbonSaving: 0, totalDistance: 0, totalRewardsEarned: 0 };
-    index[row.name].totalCarbonSaving += row.totalCarbonSaving;
-    index[row.name].totalDistance += row.totalDistance;
-    index[row.name].totalRewardsEarned += row.totalRewardsEarned;
-
-    return index;
-  }, {});
-
-  console.log(data);
-  return (
-    <BarChart
-      width={1376}
-      height={400}
-      data={Object.values(data)}
-      margin={{
-        top: 5, right: 30, left: 20, bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar name="Carbon Saving" dataKey="totalCarbonSaving" fill="#ffc658" />
-      <Bar name="Distance" dataKey="totalDistance" fill="#82ca9d" />
-      <Bar name="Rewards" dataKey="totalRewardsEarned" fill="#8884d8" />
-    </BarChart>
-  )
-}
-
-function renderChart(apiData: any[]) {
-  const colours = ["#8884d8", "#82ca9d"];
-  const data = apiData.reduce((index, row) => {
-    index[row.date] = index[row.date] || { date: row.date };
-    index[row.date][row.name] = row.totalCarbonSaving;
-
-    return index;
-  }, {});
-  const names = apiData.reduce((index, row) => {
-    index[row.name] = true;
-
-    return index;
-  }, {});
-
-  const bars = Object.keys(names).map((name, i) => <Bar dataKey={name} fill={colours[i % colours.length]} />);
-
-
-  console.log(data);
-  return (
-    <BarChart
-      width={1376}
-      height={400}
-      data={Object.values(data)}
-      margin={{
-        top: 5, right: 30, left: 20, bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="date" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      { Object.values(bars) }
-    </BarChart>
-  )
-}
-
 export interface DashboardPageProps {
   api: AxiosInstance
+}
+
+interface ReportRow {
+  date: string,
+  name: string,
+  totalCarbonSaving: number,
+  totalDistance: number,
+  totalRewardsEarned: number
 }
